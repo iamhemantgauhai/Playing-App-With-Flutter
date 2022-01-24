158c53c1bb6b46eff97fb285ae383a5d8c30ed00
iamhemantgauhai <hemant20@navgurukul.org>
2022-01-24 18:30:46 +0530

    created the app

---
+++ android/app/src/main/java/appscrip/co/hey_hello/MainActivity.java
@@ -0,0 +1,6 @@
+package appscrip.co.hey_hello;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(
+              fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.bold),
+          bodyText2: TextStyle(
+            fontSize: 60.0,
+            color: Colors.white,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+      ),
+      home: const HomeView(title: 'Flutter : Increase & Decrease'),
+    );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  final String title;
+  const HomeView({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _counter = 0;
+
+  void _increaseCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _refreshCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  void _decreaseCounter() {
+    if (_counter > 0) {
+      setState(() {
+        _counter--;
+      });
+    }
+  }
+
+  final List<Color> _color = [
+    Colors.blueAccent,
+    Colors.redAccent,
+    Colors.deepPurpleAccent,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: _color[_counter % _color.length],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Text(widget.title),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: _refreshCounter,
+            child: const Icon(Icons.refresh),
+          ),
+          const SizedBox(
+            width: 10.0,
+          ),
+          FloatingActionButton(
+            onPressed: _decreaseCounter,
+            child: const Icon(Icons.remove),
+          ),
+          const SizedBox(
+            width: 10.0,
+          ),
+          FloatingActionButton(
+            onPressed: _increaseCounter,
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Tapped button this many times:',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
